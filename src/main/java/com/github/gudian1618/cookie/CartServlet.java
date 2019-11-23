0a00052c82e6b4bca78343109e46f74a56a3c175
gudian1618 <1761844211@qq.com>
2019-11-23 22:37:43 +0800

    新建

--- src/main/java/com/github/gudian1618/cookie/CartServlet.java
+++ src/main/java/com/github/gudian1618/cookie/CartServlet.java
@@ -2,6 +2,7 @@ package com.github.gudian1618.cookie;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -21,10 +22,12 @@ public class CartServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         System.out.println("CartServlet.doGet()");
+        // 0.处理响应正文乱码
+        response.setContentType("text/html;charset=utf-8");
         // 1.获取要加入购物车的商品信息(prod)
-
+        String prod = request.getParameter("prod");
         // 2.创建cookie对象,将商品保存到cookie中
-
+        Cookie cookie = new Cookie("prod", prod);
         // 3.将cookie发送给浏览器,让浏览器保存cookie信息
 
         // 4.做出响应
